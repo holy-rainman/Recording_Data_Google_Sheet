56ea583ce12c981e75a2c4f956307f9e45c20c25
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-01-04 23:33:31 +0800

    Update record_DHT_GS.ino

--- record_DHT_GS.ino
+++ record_DHT_GS.ino
@@ -7,20 +7,20 @@
 #include "DHTesp.h"
 DHTesp dht;
 
-#define WIFI_SSID "JHChua@unifi"
-#define WIFI_PASSWORD "adamdanial123"
+#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
+#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
 
 // Google Project ID
-#define PROJECT_ID "uthm-aim"
+#define PROJECT_ID "REPLACE_WITH_YOUR_PROJECT_ID"
 
 // Service Account's client email
-#define CLIENT_EMAIL "uthm-aim@uthm-aim.iam.gserviceaccount.com"
+#define CLIENT_EMAIL "REPLACE_WITH_YOUR_CLIENT_EMAIL"
 
 // Service Account's private key
-const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCljneEKazXa70e\nDsAtjhrJ15iX6JHoyxLWSKsn/SwcMayJxII6ooTqE7nNu3D7MN3lIdFGYfXpsE4M\nWSvxRULiF1HzyDFPQ98a87aoUEEU8y9Kyj5BUTCKzA4EEyDOJg3VeDjfoyAhGS6f\n0tWqx3AvyEpub164XdTCv4Jc65GNzd/f8HAkd/86C2nbtO2maU9z4wPyNgOkNFnZ\nw2tsmhJJRY5W1RpEFL9+M4+400bej6wxCyogTJrwI1lDSIeAAOlVaD4VWZm1JkG4\nKE+KCuLZSOigCxfMUhjc6OlP/sSGgAlSD4iKnER6WKwJPLhqkj5+TKt5vdb6hkN5\nK7yd8oP3AgMBAAECggEANxi/MLY35edepGqvTVneE29TQrbtHCa9KaA8ic3pSKbF\nb4H8/yuIBWi0LMIlOwuLxU4oWf6O+wsoJlbk12I//INbTEDDHEiPruYN010GE8VM\n51yANHejKoX3YibFg6XJc9ITR2X/8tPxHtJpNnEyJkzHbJJW2Q+tF3TpmyfwpFqm\n2j1YCzUau8ZApRJ0t2S8H9YqXSPbQ2dwiQgM6Wik6sVDvd8twCYvTq4lHGsv25PS\n8Q5oKzw2nvTSgt0vT3rlFiDGnxOltcZX5kNN1p25MLiCp3ONUT+u8SIseQx4Vaut\n4XYQkDejQEYskgWqLHMPVpAyxY3JhyeXTo3hfeyXoQKBgQDZnUUqOpyKn69oB9E/\nM3doD1s2L/GsbJV5smKp4kdfatBW2pLizXU7Ik52FjMmqHcQhqiv2wztAYIlrsbR\nJcAk6VDajXK8UXwWhgfBySZ9IRmf6GUCDC5a9aR2eAsj+z4y97oRbR+42eIbbMUg\nzqbZNjlNUR7bt/GzBnPhmskIVwKBgQDCwnDXNgOZKqXywMMSRaUO+HjTCwHfX+s3\nf56E7DN8JmCHgQNlc+mau2698oHXBOz9zc3j8PUf7ietkj6zeZVwyWHiMQwauvAi\nO6ZUjsjCBIdYijjsYln0dhZmula11wc5xf8Txxcs14lFNpnN/MY7P4xHAmo1eQRt\nuhvvOf+dYQKBgASonEFyLcW982hbunwK4wSER6SaVCcnz+9iBUCzymldhQZG+ZwH\nX/obGujmQsQSGTJuX5AcNWOhg6LA4hq6HynrIML9AjbL28czqVjZsIw/OCg3Nmpb\nzmY+Gf47RanIiCVZLk+PMX2olHGX24R/SA0gXwurr+huQKtfeP2cba9TAoGAKWP5\nZ0NrcFnH+tdMnc1/TrcvtmpYc4iHDLxqjAVGy9O8IBAjEKSZuPCskGHBhljdpPLg\nFtlWvvp2JEU4Xhwwzk9+JfwWfnfMIPpcVwwhTlp4CTEW9+XPCE0wZZSkaPDR8E1L\n8IAXRMhpu/PmrAhUeLMAtPama8bUUh2DYkwJLwECgYEAiy1V0cYgQgA3m+9YeUT3\nv4FG8fhyER01UqPLt/3xlnhliQnkI8N4betzdh5Z0GnWkf0MXvBU07zd3FbENugY\nKWR/LKdp6wXiHfstvqzjFRyoghezbV073yQ68tpluF51jixpjBk8lBO0FpawZqo7\nxPJylaN2av4PDwaO7VJHwc4=\n-----END PRIVATE KEY-----\n";
+const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\ REPLACE_WITH_YOUR_PRIVATE_KEY\n-----END PRIVATE KEY-----\n";
 
 // The ID of the spreadsheet where you'll publish the data
-const char spreadsheetId[] = "1BKjyRPRSwTbcmuXfgUbJCzPYMgqjOJcYQeaZmL8xXqU";
+const char spreadsheetId[] = "YOUR_SPREADSHEET_ID";
 
 // Timer variables
 unsigned long lastTime = 0;  
